8e44425bad208968fee361d6ee0f5664ea267444
Pamela Fracassa <pamela.fracassa@windtre.it>
2022-08-02 15:38:59 +0200

    add Dasboard Toggle

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,31 +1,67 @@
 import { connect } from 'react-redux';
 import './Dashboard.css';
 import Question from './Question';
+import { useState } from 'react';
 
 const Dashboard = (props) => {
+  const [checkedNewQuestion, setCeckedNewQuestion] = useState(true);
+  const [checkedAnswerdQuestion, setCeckedAnsweredQuestion] = useState(false);
+
+  const handleChangeCheckNewQuestion = () => {
+    setCeckedNewQuestion(!checkedNewQuestion);
+  };
+  const handleChangeCheckAnsweredQuestion = () => {
+    setCeckedAnsweredQuestion(!checkedAnswerdQuestion);
+  };
   return (
     <div className='wrapper-container'>
       <h1 className='gradient-text'> Polls Dashboard</h1>
-      <h3> New Questions: </h3>
-      <ul className='questions-list'>
-        {props.questionsIds
-          .filter((item) => !props.userAnswersIds.includes(item))
-          .map((id) => (
-            <li key={id}>
-              <Question id={id} />
-            </li>
-          ))}
-      </ul>
-      <h3> Answered Questions: </h3>
-      <ul className='questions-list'>
-        {props.questionsIds
-          .filter((item) => props.userAnswersIds.includes(item))
-          .map((id) => (
-            <li key={id}>
-              <Question id={id} />
-            </li>
-          ))}
-      </ul>
+      <h3 className='title-check'> New Questions: </h3>
+      <label className='switch'>
+        <input
+          id='new-question-check'
+          type='checkbox'
+          checked={checkedNewQuestion}
+          onChange={handleChangeCheckNewQuestion}
+        />
+        <span className='slider round'></span>
+      </label>
+      {checkedNewQuestion && (
+        <div>
+          <ul className='questions-list'>
+            {props.questionsIds
+              .filter((item) => !props.userAnswersIds.includes(item))
+              .map((id) => (
+                <li key={id}>
+                  <Question id={id} />
+                </li>
+              ))}
+          </ul>
+        </div>
+      )}
+      <h3 className='title-check'> Answered Questions: </h3>
+      <label className='switch'>
+        <input
+          id='answerd-question-check'
+          type='checkbox'
+          checked={checkedAnswerdQuestion}
+          onChange={handleChangeCheckAnsweredQuestion}
+        />
+        <span className='slider round'></span>
+      </label>
+      {checkedAnswerdQuestion && (
+        <div>
+          <ul className='questions-list'>
+            {props.questionsIds
+              .filter((item) => props.userAnswersIds.includes(item))
+              .map((id) => (
+                <li key={id}>
+                  <Question id={id} />
+                </li>
+              ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
